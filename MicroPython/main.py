31f37b5c830fe030716053b88d03b37d2054241e
Elliott-Roach <elliott.roach@mths.ca>
2024-10-07 15:16:00 +0000

    add brakets

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,14 +7,14 @@ This module is a Micro:bit MicroPython program
 from microbit import *
 
 display.clear
-pin16.__init__0
+pin16.__init__(0)
 sleep(1000)
 
 while True:
     if button_a.is_pressed():
-        pin16.__init__1
+        pin16.__init__(1)
         Image.YES
 
     if button_b.is_pressed():
-        pin16.__init__0
+        pin16.__init__(0)
         Image.NO

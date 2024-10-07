473aaa95398a5f3c34b48704e73498047c225021
Elliott-Roach <elliott.roach@mths.ca>
2024-10-07 15:12:43 +0000

    on off code changed

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,14 +7,14 @@ This module is a Micro:bit MicroPython program
 from microbit import *
 
 display.clear
-pin16(0)
+input.pin16(0)
 sleep(1000)
 
 while True:
     if button_a.is_pressed():
-        pin16(1)
+        input.pin16(1)
         Image.YES
 
     if button_b.is_pressed():
-        pin16(0)
+        input.pin16(0)
         Image.NO

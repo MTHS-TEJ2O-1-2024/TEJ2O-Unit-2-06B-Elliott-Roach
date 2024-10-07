923708e9911a65c13f1ee17d1896eedb21c6cd76
Elliott-Roach <elliott.roach@mths.ca>
2024-10-07 15:17:59 +0000

    made pin a varyoubul

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,14 +7,14 @@ This module is a Micro:bit MicroPython program
 from microbit import *
 
 display.clear
-pin16.__init__(0)
+pin16 = 0
 sleep(1000)
 
 while True:
     if button_a.is_pressed():
-        pin16.__init__(1)
+        pin16 = 1
         Image.YES
 
     if button_b.is_pressed():
-        pin16.__init__(0)
+        pin16 = 0
         Image.NO
